b600bae4e7d9ea1accc041a5ed1c8f5ebd45dd92
Chris Kelly <bigkahuna1uk-git@gmail.com>
2017-02-05 13:55:43 +0000

    Added skeleton lein project for calculator

---
+++ calculator/project.clj
@@ -0,0 +1,6 @@
+(defproject calculator "0.1.0-SNAPSHOT"
+  :description "BDD Calculator example"
+  :url "https://github.com/bigkahuna1uk/clojure-bdd"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
